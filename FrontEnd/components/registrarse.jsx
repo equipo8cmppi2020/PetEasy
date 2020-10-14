66602087e84bb878597504f4f7fda6e5e4130f9c
Enmanuel Dominguez <66426641+EnmanuelDominguez@users.noreply.github.com>
2020-10-14 11:46:21 -0500

    Pantallas Pantalla de Registro e inicio de sesión agregadas

--- FrontEnd/components/registrarse.jsx
+++ FrontEnd/components/registrarse.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./styles.css";
 
-const registrarse = () => {
-  return <div></div>;
-};
-
+export default function App() {
+  return (
+    <div className="App">
+      <h5>PetEasy</h5>
+      <h1>
+        <img
+          src="https://i.imgur.com/rVTvJoV.png"
+          alt=""
+          classname=""
+          width="230"
+          height="230"
+        />
+      </h1>
+      <h5 class="textreference">Correo*</h5>
+      <p></p>
+      <textarea class="textbox">Inserte su correo aquí</textarea>
+      <p></p>
+      <h5 class="textreference">Contraseña*</h5>
+      <p></p>
+      <textarea class="textbox">Inserte su contraseña aquí</textarea>
+      <p></p>
+      <h5 class="textreference">Confirmar contraseña*</h5>
+      <p></p>
+      <textarea class="textbox">Confirme su contraseña aquí</textarea>
+      <p></p>
+      <button class="boton_registro">Registrarse</button>{" "}
+      <button class="boton_registro">Volver</button>
+    </div>
+  );
+}
 export default registrarse;
